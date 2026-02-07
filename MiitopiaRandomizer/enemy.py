330e7280c778f0d8aa62353ec0f34854d45aaf68
otix56 <dreamland.yt717@gmail.com>
2026-02-07 07:39:01 -0300

    Create enemy.py

---
+++ MiitopiaRandomizer/enemy.py
@@ -0,0 +1,28 @@
+import csv
+import random
+
+
+from MiitopiaRandomizer.const import enemy_limit_switch, enemy_limit_3ds
+from MiitopiaRandomizer.util import get_csv_rows_from_input_sarc‎
+
+
+def randomize_enemy ():
+    curr_level = next(enemy[1] for enemy in enemies if enemy[0] == enemy_name)
+    random_level_offset = random.randint(-2, 2)
+    # Make sure the level doesn't go below 1
+    random_level = max([curr_level + random_level_offset, 50])
+    # Get all enemies with this new level
+    new_enemies = [enemy[0] for enemy in enemies if enemy[1] == random_level]
+    new_enemy = random.choice(new_enemies)
+    while new_enemy == 'Goblin0_Dish':
+    new_enemy = random.choice(new_enemies)
+
+face_list: list[str] = []
+for face_list in get_csv_rows_from_input_sarc‎('npc.sarc', 'NPCList.csv')
+#do not use "Hero"
+if face_list[0].startswith('Hero') == -1:
+  facelist.append(face_list[0])
+
+
+def randomize_face ()
+    random.choice(face_list)
